53edae19d4d1794db8d8aa2b6f8ee197eace182e
Francis Aweenagua <faweenagua@gmail.com>
2023-09-29 11:03:54 +0000

    Contour detection added

--- obstacleAvoidance.py
+++ obstacleAvoidance.py
@@ -3,6 +3,13 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
+def getContours(img):
+    # contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours,hierarchy = cv2.findContours(img, 1, 2)
+    for cnt in contours:
+        cv2.drawContours(img, cnt, -1, (255, 145, 78), 2)
+
+
 
 while True:
     _,img = cap.read()
@@ -10,8 +17,14 @@ while True:
     img = cv2.resize(img, (480,360))
     # img = cv2.flip(img, 0)
 
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (7,7), 1)
+    imgCanny = cv2.Canny(imgBlur, 50, 50)
+    getContours(imgCanny)
 
     cv2.imshow("Output", img)
-    # cv2.imshow("Path", imgThresh)
+    cv2.imshow("Blur", imgBlur)
+    cv2.imshow("Blur", imgCanny)
+
 
     cv2.waitKey(1)
